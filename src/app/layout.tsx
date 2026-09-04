c4e0001a8cd90d3b562f861aaa5816ba16047a68
József Tar <szurilo@gmail.com>
2026-09-04 15:01:14 +0200

    Update widget script data-key in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <SiteHeader />
         <main className="flex-1">{children}</main>
         <SiteFooter />
-        <script src="http://localhost:3000/widget.js" data-key="4f5d6785c53ca7f2d710f363f71c448d" async></script>
+        <script src="http://localhost:3000/widget.js" data-key="89083c1736110203b186691e481894ef" async></script>
       </body>
     </html>
   );
